f15be15c9e4932721c27f9f2e0d3410087576cf4
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-11-22 21:43:12 +0200

    Initial Commit

---
+++ 015_VenturaMenuBar/_15_VenturaMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  _15_VenturaMenuBarApp.swift
+//  015_VenturaMenuBar
+//
+//  Created by nikita on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct _15_VenturaMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
